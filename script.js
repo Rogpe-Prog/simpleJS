c7ae49aba71a27c2d30711b553916511bed9a250
Rogpe-Prog <paulo.rogpe@gmail.com>
2022-06-12 12:26:13 -0300

    include, startsWith and endsWith

---
+++ script.js
@@ -0,0 +1,18 @@
+
+// includes, startsWith, endsWith
+
+let nomes = ["Dart", "Homer", "Luke"];
+
+console.log(nomes.includes("Morg"));
+
+if(nomes.includes("Jitt")){
+  console.log("It is on List")
+}else{
+  console.log("It isn't on List")
+}
+
+let nome = "Luke";
+
+console.log(nome.startsWith("Dar"))
+
+console.log(nome.endsWith("ke"))
